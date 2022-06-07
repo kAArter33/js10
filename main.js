ac1363090593d2d4a868b61968cdc3b06f6349ec
kbaginski <106392277+kAArter33@users.noreply.github.com>
2022-06-07 11:30:37 +0200

    poprawka 2

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 let num1 = 2;
 let num2 = 5;
 
-function liczba() {
+function liczba(par1,par2) {
     if (num1 > num2) {
+        console.log("parametr 1 jest wiekszy od parametru 2");
+        return par1;
     }
-    return 5;
+    else
+    {
+        console.log("parametr 2 jest wiekszy od parametru 1")
+        return par2;
+    }
 }
-console.log(liczba());
+console.log(liczba(num1,num2));
